9945b5d512a705a0921cd31c2028c9ebe20dfa5e
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-09 05:52:27 +0800

    simple tcp server and sending using net module

---
+++ tcp-server/simple-server.js
@@ -0,0 +1,13 @@
+const net = require('net');
+
+const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    console.log(data.toString('utf-8'));
+  });
+});
+
+const port = 3000;
+const ip_address = '127.0.0.1';
+server.listen(port, ip_address, () =>
+  console.log('Listening on ', server.address())
+);
