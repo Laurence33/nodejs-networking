3f8ab454d22d8a9b2587eb04972744f50f5ac68d
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-09 10:35:40 +0800

    multi client

--- chat-app/server.js
+++ chat-app/server.js
@@ -2,11 +2,16 @@ const http = require('http');
 const net = require('net');
 
 const server = net.createServer();
-
+const clients = [];
 server.on('connection', (socket) => {
   console.log('New connection to the server');
+  clients.push(socket);
   socket.write('Hello client!');
   socket.on('error', (err) => console.log(`Client error`, err.message));
+  socket.on('data', (data) => {
+    console.log(`Client sent: ${data}`);
+    clients.forEach((s) => s.write(data));
+  });
 });
 
 const port = 3000;
