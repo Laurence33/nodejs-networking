bb3ca74eb72ad73fcd387fba6d65ac5217583479
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-09 10:35:29 +0800

    readline to input client message

--- chat-app/client.js
+++ chat-app/client.js
@@ -1,19 +1,29 @@
 const net = require('net');
+const readline = require('readline/promises');
 
-const client = net.createConnection(
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const socket = net.createConnection(
   {
     host: '127.0.0.1',
     port: 3000,
   },
-  () => console.log(`Connected to server!`)
+  async () => {
+    console.log(`Connected to server!`);
+    const message = await rl.question('Enter message >');
+    socket.write(message);
+  }
 );
 
-client.on('data', function (data) {
-  console.log(`server sent: ${data}`);
+socket.on('data', function (data) {
+  console.log(data.toString('utf-8'));
 });
 
-client.on('end', () => console.log(`Connection ended.`));
-client.on('error', (err) => {
+socket.on('end', () => console.log(`Connection ended.`));
+socket.on('error', (err) => {
   if (err.message.includes('ECONNRESET')) {
     console.log('Connection was reset');
   } else {
