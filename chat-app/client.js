cd06f9aefce69707319024af7a59f4128004c4cd
Laurence Cortez <laurencecortez26.lc@gmail.com>
2023-09-09 11:07:33 +0800

    better client ui

--- chat-app/client.js
+++ chat-app/client.js
@@ -6,6 +6,18 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = async (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => resolve());
+  });
+};
+
+const moveCursor = async (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(0, -1, () => resolve());
+  });
+};
+
 const socket = net.createConnection(
   {
     host: '127.0.0.1',
@@ -13,14 +25,25 @@ const socket = net.createConnection(
   },
   async () => {
     console.log(`Connected to server!`);
-    const message = await rl.question('Enter message >');
-    socket.write(message);
+    const ask = async () => {
+      const message = await rl.question('Enter message >');
+      // move cursor up
+      await moveCursor(0, -1);
+      // clear whole line
+      await clearLine(0);
+      socket.write(message);
+    };
+    await ask();
+
+    socket.on('data', async function (data) {
+      console.log();
+      await moveCursor(0, -1);
+      await clearLine(0);
+      console.log(data.toString('utf-8'));
+      ask();
+    });
   }
 );
-
-socket.on('data', function (data) {
-  console.log(data.toString('utf-8'));
-});
 
 socket.on('end', () => console.log(`Connection ended.`));
 socket.on('error', (err) => {
